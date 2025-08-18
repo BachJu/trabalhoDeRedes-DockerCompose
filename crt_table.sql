23749064e779ab90fc2e72b5e089604c7263e957
BachJu <bachjuliana.julianasbach@gmail.com>
2025-08-18 18:34:02 -0400

    Quinto commit: adaptando o crt_table.sql para mysql

--- crt_table.sql
+++ crt_table.sql
@@ -1,46 +1,50 @@
-/* Lógico_1: */
-
-CREATE TABLE Funcionario (
-    Id SERIAL PRIMARY KEY,
-    Nome VARCHAR,
-    Email VARCHAR,
-    Telefone INTEGER,
-    CEP VARCHAR
+CREATE DATABASE IF NOT EXISTS trabalho;
+USE trabalho;
+CREATE TABLE IF NOT EXISTS Funcionario(
+    id int(25) AUTO_INCREMENT,
+    nome VARCHAR(30),
+    email VARCHAR(50),
+    telefone INTEGER,
+    cep VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Venda (
-    IdVenda SERIAL PRIMARY KEY,
-    Data NUMERIC,
-    Status VARCHAR,
-    fk_Funcionario_Id INTEGER
+CREATE TABLE IF NOT EXISTS Venda (
+    idVenda int(25) AUTO_INCREMENT,
+    data DATE DEFAULT CURDATE(),
+    status BOOLEAN,
+    fk_funcionario_id INTEGER,
+    PRIMARY KEY(idVenda)
 );
 
-CREATE TABLE Produto (
-    IdProduto SERIAL PRIMARY KEY,
-    Nome VARCHAR,
-    Preco NUMERIC(10, 2),
-    Quantidade INTEGER
+CREATE TABLE IF NOT EXISTS Produto (
+    idProduto int(25) AUTO_INCREMENT,
+    nome VARCHAR(30),
+    preco FLOAT(10, 2),
+    quantidade INTEGER,
+    PRIMARY KEY(idProduto)
 );
 
-CREATE TABLE Tem (
-    fk_Produto_IdProduto INTEGER,
-    fk_Venda_IdVenda INTEGER,
-    QuantidadeVendida INTEGER,
-    PrecoNaVenda NUMERIC(10, 2),
+CREATE TABLE IF NOT EXISTS Tem (
+    idTem int(25) AUTO_INCREMENT,
+    fk_produto_idProduto INTEGER,
+    fk_venda_idVenda INTEGER,
+    quantidadeVendida INTEGER,
+    precoNaVenda FLOAT(10, 2),
     Desconto NUMERIC(10, 2)
 );
  
 ALTER TABLE Venda ADD CONSTRAINT FK_Venda_2
-    FOREIGN KEY (fk_Funcionario_Id)
-    REFERENCES Funcionario (Id)
+    FOREIGN KEY (fk_funcionario_id)
+    REFERENCES Funcionario (id)
     ON DELETE RESTRICT;
  
 ALTER TABLE Tem ADD CONSTRAINT FK_Tem_1
-    FOREIGN KEY (fk_Produto_IdProduto)
-    REFERENCES Produto (IdProduto)
+    FOREIGN KEY (fk_produto_idProduto)
+    REFERENCES Produto (idProduto)
     ON DELETE RESTRICT;
  
 ALTER TABLE Tem ADD CONSTRAINT FK_Tem_2
-    FOREIGN KEY (fk_Venda_IdVenda)
-    REFERENCES Venda (IdVenda)
+    FOREIGN KEY (fk_venda_idVenda)
+    REFERENCES Venda (idVenda)
     ON DELETE RESTRICT;
